b2af6983cc9d0d5191cb6ed620ef43ad31d21a6d
DanThemes <danthemes@gmail.com>
2022-10-07 21:15:16 +0300

    update

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use(function (req, res, next) {
 const { Server } = require('socket.io');
 const io = new Server(server, {
   cors: {
-    origin: 'https://danthemes.com',
+    origin: 'https://danthemes.com/react/x0/',
     credentials: true,
     methods: ['GET', 'POST']
   }
